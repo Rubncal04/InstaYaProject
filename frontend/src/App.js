4a0ac0711dd91ccff1640bcf42a5ffff2c55f5ea
Ruben Gomez <rubncal.9501@gmail.com>
2022-11-30 11:33:33 -0500

    Add views for order

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import SignIn from './components/SignIn';
 import Welcome from './components/Welcome';
+import ShowOrder from './components/ShowOrder';
+import NewOrder from './components/NewOrder';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
           <Route path='/' element={<Login/>}/>
           <Route path='/sign-in' element={<SignIn/>}/>
           <Route path='/welcome/:id' element={<Welcome/>}/>
+          <Route path='/:id/orders/:order_id' element={<ShowOrder/>}/>
+          <Route path='/:id/orders' element={<NewOrder/>}/>
         </Routes>
       </div>
     </div>
